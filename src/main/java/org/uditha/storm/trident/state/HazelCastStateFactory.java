4b8d7a12368c6c468da29d9b9067e296e6582982
Uditha Bandara Wijerathna <udithabnd@gmail.com>
2014-09-22 01:37:00 +0530

    Experimenting HazelCast

---
+++ src/main/java/org/uditha/storm/trident/state/HazelCastStateFactory.java
@@ -0,0 +1,19 @@
+package org.uditha.storm.trident.state;
+
+
+import backtype.storm.task.IMetricsContext;
+import storm.trident.state.StateFactory;
+import storm.trident.state.TransactionalValue;
+import storm.trident.state.map.*;
+
+import java.util.Map;
+
+public class HazelCastStateFactory<T> implements StateFactory {
+
+
+    @Override
+    public MapState<T> makeState(Map conf, IMetricsContext metrics, int partitionIndex, int numPartitions) {
+        IBackingMap im = new HazelCastState<T>(new HazelCastHandler<TransactionalValue<T>>());
+        return TransactionalMap.build(im);
+    }
+}
